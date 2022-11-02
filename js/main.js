88ad83044fcd5380c777ed8dcab82964160438ee
Matthew Chau <matthewwchau@gmail.com>
2022-11-02 02:21:40 +0000

    Put the form's input values into a new object. #1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,28 @@
 
 function newPhoto(event) {
   $img.src = ($input.value);
-
 }
 
 var $input = document.getElementById('url-link');
 $input.addEventListener('input', newPhoto);
 var $img = document.getElementById('photo');
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  var title = $title.value;
+  var urlLink = $input.value;
+  var textNotes = $notes.value;
+  var object = {
+    submitTitle: title,
+    submitUrl: urlLink,
+    submitNotes: textNotes
+  };
+  return object;
+}
+
+var $submit = document.getElementById('submitForm');
+$submit.addEventListener('submit', formSubmit);
+
+var $title = document.getElementById('title');
+var $notes = document.getElementById('notes');
